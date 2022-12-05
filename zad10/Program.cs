0db5130eb9718fc4058df0f435eb5d3781069819
Benny Elashkin <pavel.elashkin@gmail.com>
2022-12-05 18:16:57 +0300

    zad10 v0

---
+++ zad10/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Напишите программу, которая принимает на вход трехзначное число и на 
+выходе показывает вторую цифру этого числа
+456->5
+782->8
+918->1
+*/
+// Метод проверки вводимых данных на число
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            return result;
+        }
+        else 
+            Console.WriteLine("Вы ввели не число");
+        if(Math.Abs(result)/1000<0)
+        {
+            return result;
+        }
+        else Console.WriteLine("Вы ввели не 3-х значное число");
+    }
+    return result;
+}
+
+
+int num = GetNumber("Введите 3-х значное число: ");
+
