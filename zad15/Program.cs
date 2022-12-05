7b20d0f828cdafab40457035a1c8d26ba31f07f9
Benny Elashkin <pavel.elashkin@gmail.com>
2022-12-05 21:42:16 +0300

    zad15 v1

---
+++ zad15/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую
+день недели, и проверяет, является ли этот день выходным
+
+645->5
+78-> третьей цифры нет
+*/
+
+// Метод проверки вводимых данных на число
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+            return result;
+        else
+            Console.WriteLine("Вы ввели не число");
+    }
+    return result;
+}
+
+//Метод проверки диапазона от 1 до 7
+void CheckNumber(int number)
+{
+    if (number < 1 || number > 7)
+        Console.Write($"Ошибка.Введенное число {number} не соответствует дню редели (вне диапазона)!");
+    else ShowNumber(number);
+}
+
+//Метод проверяет выходной или нет
+void ShowNumber(int num)
+{
+    if (num == 6 || num == 7)
+        Console.WriteLine($"День № {num} - выходной!");
+    else Console.WriteLine($"День № {num} - рабочий!");
+}
+
+int num = GetNumber("Введите число от 1 до 7: ");
+CheckNumber(num);
